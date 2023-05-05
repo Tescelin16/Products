08db880c8fe50e7b7df06505e5233b064570454c
Daniel Cocco <daniel.edg.cocco@gmail.com>
2023-05-05 20:49:36 -0300

    Adding packages

---
+++ src/main/java/com/products/dto/ProductoDto.java
@@ -0,0 +1,15 @@
+package com.products.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductoDto {
+
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private String calidad;
+    
+}
---
+++ src/main/java/com/products/service/implementation/ProductoServiceImplementation.java
@@ -0,0 +1,9 @@
+package com.products.service.implementation;
+
+import com.products.service.ProductoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductoServiceImplementation implements ProductoService {
+
+}
