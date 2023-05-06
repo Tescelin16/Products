6490c889daf59ea3e267e9b845210f05891dba03
Daniel Cocco <daniel.edg.cocco@gmail.com>
2023-05-05 23:03:11 -0300

    fixing pom

--- src/main/java/com/products/entity/Producto.java
+++ src/main/java/com/products/entity/Producto.java
@@ -1,12 +1,14 @@
 package com.products.entity;
 
 import com.products.dto.ProductoDto;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 
 @Entity
